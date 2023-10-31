a848e7d142eb7d49157b281f9df8ff2f08b6ce14
emna-naija-pro <100705776+emna-naija-pro@users.noreply.github.com>
2023-10-31 19:20:30 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+ReactDOM.render(
+  <div>
+    <h1> hello world!</h1>
+    <p>new</p>
+  </div>,
+  document.getElementById("root")
+);
+//with jsx we can use html in javascript code  to avoid too much java script code like document.innerhtml ...
